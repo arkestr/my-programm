94084ab8e8e21bf5043fc4f57e5f89d4dd496284
arkestr <russkyyy@mail.ru>
2014-12-22 00:18:55 +0400

    program 3

---
+++ Name.java
@@ -0,0 +1,12 @@
+public class Name {
+    public void name(){
+        String name1, name2, result = "тезки";
+        name1 = "сергей";
+        name2 = "андрей";
+        if (name1 == name2) {
+            System.out.println(result);
+        }
+
+    }
+}
+
