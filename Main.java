6cc303b9182eeef5065e54afa864d07c3145ad08
arkestr <russkyyy@mail.ru>
2014-12-22 14:31:39 +0400

    findSymbol

--- Main.java
+++ Main.java
@@ -1,24 +1,23 @@
+
 public class Main {
     public static void main(String[] args) {
-        IfElse newLas = new IfElse();
-        newLas.min();
+        String searchMe = "моя программа работает";
+        int k = 0;
+        int max = searchMe.length();
+        char symb = 'о';
+        boolean find = false;
+        for (int i = 0; i < max; i++) {
+            if (searchMe.charAt(i) == symb) {
+                k = k + 1;
+                if (k >= 2) {
+                    find = true;
+                    break;
+                }
+            }
+        }
+        if (find)
+            System.out.println("Символы '" + symb + "' найдены в строке");
+        else
+            System.out.println("Символов '" + symb + "' меньше чем 2");
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
