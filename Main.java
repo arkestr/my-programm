cf49514674e94011e3b581bb4db16c10bc941f32
arkestr <russkyyy@mail.ru>
2014-12-24 21:32:41 +0400

    смена элементов массива

--- Main.java
+++ Main.java
@@ -1,23 +1,30 @@
-
 public class Main {
     public static void main(String[] args) {
-        String searchMe = "моя программа работает";
-        int k = 0;
-        int max = searchMe.length();
-        char symb = 'о';
-        boolean find = false;
-        for (int i = 0; i < max; i++) {
-            if (searchMe.charAt(i) == symb) {
-                k = k + 1;
-                if (k >= 2) {
-                    find = true;
-                    break;
-                }
+        int[] Array = {-6, -5, 0, 6, 8};
+        int min = 0;
+        min = Array[0];
+        int max = 0;
+        int iMax = 0;
+        max = Array[0];
+        for (int i = 0; i < Array.length; i++) {
+            if (min >= Array[i]) {
+                min = Array[i];
+            }
+            if (max <= Array[i]) {
+                max = Array[i];
+                iMax = i;
             }
         }
-        if (find)
-            System.out.println("Символы '" + symb + "' найдены в строке");
-        else
-            System.out.println("Символов '" + symb + "' меньше чем 2");
+        int max1 = max;
+        for (int i = 0; i < Array.length; i++) {
+            if (min == Array[i]) {
+                Array[i] = max;
+            }
+            if (max == Array[i]) {
+                Array[iMax] = min;
+            }
+            System.out.println(Array[i]);
+        }
+        System.out.println(min);
     }
 }
